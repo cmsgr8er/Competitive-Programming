b789e4bd0177bec80741d5e8ef1794feb164dc79
cmsgr8er <79183840+cmsgr8er@users.noreply.github.com>
2021-07-08 00:51:21 -0500

    Update lightson.cpp

--- Silver/DFS/FloodFill/lightson.cpp
+++ Silver/DFS/FloodFill/lightson.cpp
@@ -3,7 +3,7 @@ using namespace std;
 vector<pair<int, int>> adj[102][102];
 bool connected[102][102], visited[102][102];
 int N, M, answer = 0;
-void flow(int r, int c) {
+void toggle(int r, int c) {
 	for (auto pair: adj[r][c]) {
 		if (!connected[get<0>(pair)][get<1>(pair)]) {
 			connected[get<0>(pair)][get<1>(pair)] = true;
@@ -14,7 +14,7 @@ void flow(int r, int c) {
 void floodfill(int r, int c) {
 	visited[r][c] = true;
 	if (r == 0 || r== N+1 || c ==0 || c== N+1) return; // out of bounds
-	flow(r,c);
+	toggle(r,c);
 	int dx[4] = {0,0,1,-1}, dy[4] = {1,-1,0,0};
 	for (int i=0; i<4; i++) {
 		if (!visited[r+dx[i]][c+dy[i]] && connected[r+dx[i]][c+dy[i]]) {
